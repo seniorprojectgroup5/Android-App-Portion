1c0dc491b3f7ce9dd2da0a3b94c4f3dee19aa507
Kalia <kaliahameiri@gmail.com>
2019-10-14 12:03:27 -0400

    Added placeholder media control buttons to  visualizer fragment

---
+++ app/src/main/java/com/example/buzzinbees/VisualizerFragment.java
@@ -0,0 +1,36 @@
+package com.example.buzzinbees;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class VisualizerFragment extends Fragment {
+
+    private VisualizerViewModel mViewModel;
+
+    public static VisualizerFragment newInstance() {
+        return new VisualizerFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.visualizer_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(VisualizerViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/buzzinbees/VisualizerViewModel.java
@@ -0,0 +1,7 @@
+package com.example.buzzinbees;
+
+import androidx.lifecycle.ViewModel;
+
+public class VisualizerViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
