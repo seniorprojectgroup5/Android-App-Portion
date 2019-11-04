0d094a6173f966fed745bc8eb099d82c3219451f
JonahJana <46599996+JonahJana@users.noreply.github.com>
2019-11-04 12:04:00 -0500

    Basic structure for our bluetooth connection
    the app can...
        - turn bluetooth off/on
        -  list all PAIRED devices (not the discoverable devices. users will have to manually pair the device the first time)

--- app/src/main/java/com/example/buzzinbees/MainActivity.java
+++ app/src/main/java/com/example/buzzinbees/MainActivity.java
@@ -10,30 +10,39 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1,b2,b3,b4;
+    Button b1,b3,b4;
     private BluetoothAdapter BA;
-    ListView lv;
+    TextView connectedDevices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        temporary buttons needed
+//      turn on bluetooth
         b1 = (Button) findViewById(R.id.button);
-        b2=(Button)findViewById(R.id.button2);
-        b3=(Button)findViewById(R.id.button3);
-        b4=(Button)findViewById(R.id.button4);
+//      list bluetooth devices
+        b3 =(Button)findViewById(R.id.button3);
+//      turn off bluetooth
+        b4 =(Button)findViewById(R.id.button4);
 
-        BA = (BluetoothAdapter) BluetoothAdapter.getDefaultAdapter();
+        connectedDevices = (TextView) findViewById(R.id.listOfDevices);
+
+//      bluetooth adapter needed for bluetooth to work
+        BA = (BluetoothAdapter)BluetoothAdapter.getDefaultAdapter();
     }
 
+//    turn bluetooth on
     public void on(View v){
         if (!BA.isEnabled()) {
             Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
@@ -44,20 +53,26 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+//    turn bluetooth off
     public void off(View v){
         BA.disable();
         Toast.makeText(getApplicationContext(), "Turned off" ,Toast.LENGTH_LONG).show();
     }
 
 
-    public  void visible(View v){
-        Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-        startActivityForResult(getVisible, 0);
-    }
-
-
+//  look for all paired devices
     public void list(View v){
-        String deviceName = BA.getName();
-        Toast.makeText(getApplicationContext(), deviceName,Toast.LENGTH_SHORT).show();
+        Set<BluetoothDevice>pairedDevices;
+        pairedDevices = BA.getBondedDevices();
+
+        String deviceName = "";
+        connectedDevices.setText(deviceName);
+
+//        looks for blutooth devices, using .getName() we get the name of the device
+        for(BluetoothDevice bt : pairedDevices) deviceName = deviceName + bt.getName() + "~~~~";
+
+//        the toast for debugging
+//        Toast.makeText(getApplicationContext(), deviceName,Toast.LENGTH_SHORT).show();
+        connectedDevices.setText(deviceName);
     }
 }
