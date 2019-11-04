c9893ec9db2addd8b146b1fb3a8419bbc6941263
JonahJana <46599996+JonahJana@users.noreply.github.com>
2019-11-04 12:26:40 -0500

    is broken rn will fix later

--- app/src/main/java/com/example/buzzinbees/MainActivity.java
+++ app/src/main/java/com/example/buzzinbees/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.buzzinbees;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.media.audiofx.Visualizer;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+
+// tutorial that was followed https://blog.stylingandroid.com/visualiser-part1/
+public class MainActivity extends AppCompatActivity implements Visualizer.OnDataCaptureListener {
+
+    private static final int REQUEST_CODE = 0;
+    static final String[] PERMISSIONS = new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.MODIFY_AUDIO_SETTINGS};
+
+    private Visualizer viz;
+    private WaveformView WFView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
