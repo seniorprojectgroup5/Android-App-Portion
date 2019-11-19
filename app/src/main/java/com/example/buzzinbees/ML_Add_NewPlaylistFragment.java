5537939b05abbdcbe4acbb4a7ca07349229bcfbb
Kalia <kaliahameiri@gmail.com>
2019-11-19 11:05:49 -0500

    Added layout for New Playlist Fragment

---
+++ app/src/main/java/com/example/buzzinbees/ML_Add_NewPlaylistFragment.java
@@ -0,0 +1,31 @@
+package com.example.buzzinbees;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ML_Add_NewPlaylistFragment extends Fragment {
+
+
+    public ML_Add_NewPlaylistFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_new_playlist,null);
+    }
+
+}
