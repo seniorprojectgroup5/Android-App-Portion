20642abdb8b185f8896412a3c09c144d2d64d2fc
DawnEggleton <dawn.eggleton@carleton.ca>
2019-11-19 09:03:36 -0500

    functional music player, preloaded song

--- app/src/main/java/com/example/buzzinbees/MainActivity.java
+++ app/src/main/java/com/example/buzzinbees/MainActivity.java
@@ -2,13 +2,164 @@ package com.example.buzzinbees;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+
+public final class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    public static final int MEDIA_RES_ID = R.raw.sanctuarytheme;
+
+    private TextView debugText;
+    private SeekBar seekbarAudio;
+    private ScrollView scrollCont;
+    private PlayerAdapter playAdapt;
+    private boolean isSeeking = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeUI();
+        initializeSeekbar();
+        initializePlaybackController();
+        Log.d(TAG, "onCreate:finished");
+
     }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        playAdapt.loadMedia(MEDIA_RES_ID);
+        Log.d(TAG, "onStart: create media player");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(isChangingConfigurations() && playAdapt.isPlaying()) {
+            Log.d(TAG, "onStop: don't release mediaplayer as screen is rotating & playing");
+        } else {
+            playAdapt.release();
+            Log.d(TAG, "onStop: release mediaplayer");
+        }
+    }
+
+    private void initializeUI() {
+        //set ids to elements
+        debugText = findViewById(R.id.text_debug);
+        Button bPlay = findViewById(R.id.button_play);
+        Button bPause = findViewById(R.id.button_pause);
+        Button bReset = findViewById(R.id.button_reset);
+        seekbarAudio = findViewById(R.id.seekbar_audio);
+        scrollCont = findViewById(R.id.scroll_container);
+
+        bPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playAdapt.pause();
+            }
+        });
+
+        bPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playAdapt.play();
+            }
+        });
+
+        bReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playAdapt.reset();
+            }
+        });
+    }
+
+    private void initializePlaybackController() {
+        MediaPlayerHolder mph = new MediaPlayerHolder(this);
+        Log.d(TAG, "initializePlaybackInfoListener: created MediaPlayerHolder");
+        mph.setPlaybackInfoListener(new PlaybackListener());
+        playAdapt = mph;
+        Log.d(TAG, "initializePlaybackController: MediaPlayerHolder progress callback set");
+    }
+
+    private void initializeSeekbar () {
+        seekbarAudio.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int userSelectPosition = 0;
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                isSeeking = true;
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser) {
+                    userSelectPosition = progress;
+                }
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                isSeeking = false;
+                playAdapt.seekTo(userSelectPosition);
+            }
+        });
+    }
+
+    public class PlaybackListener extends PlaybackInfoListener {
+        @Override
+        public void onDurationChanged(int duration) {
+            seekbarAudio.setMax(duration);
+            Log.d(TAG, String.format("setPlaybackDuration: metMax(%d)", duration));
+        }
+
+        @Override
+        public void onPositionChanged(int position) {
+            if(!isSeeking) {
+                seekbarAudio.setProgress(position, true);
+                Log.d(TAG, String.format("setPlaybackPosition: setProgress(%d)", position));
+            }
+        }
+
+        @Override
+        public void onStateChanged(@State int state) {
+            String stateToString = PlaybackInfoListener.convertStateToString(state);
+            onLogUpdated(String.format("onStateChange(%s)", stateToString));
+        }
+
+        @Override
+        public void onPlaybackCompleted() {
+            //empty function
+        }
+
+        @Override
+        public void onLogUpdated(String message) {
+            if(debugText != null) {
+                debugText.append(message);
+                debugText.append("\n");
+                //move scroll container focus to the end
+                scrollCont.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        scrollCont.fullScroll(ScrollView.FOCUS_DOWN);
+                    }
+                });
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/buzzinbees/MediaPlayerHolder.java
@@ -0,0 +1,193 @@
+package com.example.buzzinbees;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public final class MediaPlayerHolder implements PlayerAdapter {
+
+    public static final int PLAYBACK_POSITION_REFRESH_INTERVAL_MS = 1000;
+
+    private final Context context;
+    private MediaPlayer mediaplayer;
+    private int rID;
+    private PlaybackInfoListener pil;
+    private ScheduledExecutorService exec;
+    private Runnable seekbarPositionUpdateTask;
+
+    public MediaPlayerHolder(Context con) {
+        context = con.getApplicationContext();
+    }
+
+
+    //sets up mediaplayer instance
+    private void initializeMediaPlayer () {
+        if (mediaplayer == null) {
+            mediaplayer = new MediaPlayer();
+            mediaplayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopUpdatingCallbackWithPosition(true);
+                    logToUI("MediaPlayer playback completed");
+                    if(mediaplayer != null) {
+                        pil.onStateChanged(PlaybackInfoListener.State.COMPLETED);
+                        pil.onPlaybackCompleted();
+                    }
+                }
+            });
+
+            logToUI("mediaplayer = new MediaPlayer()");
+        }
+    }
+
+    public void setPlaybackInfoListener(PlaybackInfoListener listener) {
+        pil = listener;
+    }
+
+
+    //implements playback control
+    @Override
+    public void loadMedia(int resourceID) {
+        rID = resourceID;
+
+        initializeMediaPlayer();
+
+        AssetFileDescriptor assetFileDescriptor = context.getResources().openRawResourceFd(rID);
+
+        try {
+            logToUI("load() {1. setDataSource}");
+            mediaplayer.setDataSource(assetFileDescriptor);
+        } catch (Exception e) {
+            logToUI(e.toString());
+        }
+
+        try {
+            logToUI("load() {2. prepare}");
+            mediaplayer.prepare();
+        } catch (Exception e) {
+            logToUI(e.toString());
+        }
+
+        initializeProgressCallback();
+        logToUI("initializeProgressCallback");
+    }
+
+    @Override
+    public void release() {
+        if(mediaplayer != null) {
+            logToUI("release() and mediaplayer = null");
+            mediaplayer.release();
+            mediaplayer = null;
+        }
+    }
+
+    @Override
+    public boolean isPlaying() {
+        if(mediaplayer != null) {
+            return mediaplayer.isPlaying();
+        }
+        return false;
+    }
+
+    @Override
+    public void play() {
+        if(mediaplayer != null && !mediaplayer.isPlaying()) {
+            logToUI(String.format("playbackStart() %s", context.getResources().getResourceEntryName(rID)));
+            mediaplayer.start();
+            if (pil != null) {
+                pil.onStateChanged(PlaybackInfoListener.State.PLAYING);
+            }
+            startUpdatingCallbackWithPosition();
+        }
+    }
+
+    @Override
+    public void reset() {
+        if(mediaplayer != null) {
+            logToUI("playbackReset()");
+            mediaplayer.reset();
+            loadMedia(rID);
+            if(pil != null) {
+                pil.onStateChanged(PlaybackInfoListener.State.RESET);
+            }
+            stopUpdatingCallbackWithPosition(true);
+        }
+    }
+
+    @Override
+    public void pause() {
+        if(mediaplayer != null && mediaplayer.isPlaying()) {
+            mediaplayer.pause();
+            if (pil != null) {
+                pil.onStateChanged(PlaybackInfoListener.State.PAUSED);
+            }
+        }
+    }
+
+    @Override
+    public void seekTo(int position) {
+        if(mediaplayer != null) {
+            logToUI(String.format("seekTo() %d ms", position));
+            mediaplayer.seekTo(position);
+        }
+    }
+
+    private void startUpdatingCallbackWithPosition() {
+        if(exec == null) {
+            exec = Executors.newSingleThreadScheduledExecutor();
+        }
+        if(seekbarPositionUpdateTask == null) {
+            seekbarPositionUpdateTask = new Runnable() {
+                @Override
+                public void run() {
+                    updateProgressCallbackTask();
+                }
+            };
+        }
+        exec.scheduleAtFixedRate(seekbarPositionUpdateTask,0,PLAYBACK_POSITION_REFRESH_INTERVAL_MS, TimeUnit.MILLISECONDS);
+    }
+
+
+    //Reports media playback position to PlaybackProgressCallback
+    private void stopUpdatingCallbackWithPosition (boolean resetUIPlaybackPosition) {
+        if(exec != null) {
+            exec.shutdownNow();
+            exec = null;
+            seekbarPositionUpdateTask = null;
+            if(resetUIPlaybackPosition && pil != null) {
+                pil.onPositionChanged(0);
+            }
+        }
+    }
+
+    private void updateProgressCallbackTask() {
+        if(mediaplayer != null && mediaplayer.isPlaying()) {
+            int currentPosition = mediaplayer.getCurrentPosition();
+            if(pil != null) {
+                pil.onPositionChanged(currentPosition);
+            }
+        }
+    }
+
+    @Override
+    public void initializeProgressCallback() {
+        final int duration = mediaplayer.getDuration();
+        if(pil != null) {
+            pil.onDurationChanged(duration);
+            pil.onPositionChanged(0);
+            logToUI(String.format("firing setPlaybackDuration(%d sec)", TimeUnit.MILLISECONDS.toSeconds(duration)));
+            logToUI("firing setPlaybackPosition(0)");
+        }
+    }
+
+    private void logToUI(String message) {
+        if(pil != null) {
+            pil.onLogUpdated(message);
+        }
+    }
+
+}
