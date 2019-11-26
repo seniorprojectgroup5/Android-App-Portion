a07856d65e2e50f8cb29e288be8889e0e1570b6d
Jonah <jonahjanakovic@cmail.carleton.ca>
2019-11-26 12:24:53 -0500

    got the basic child fragment functionality working
    there is an error in the child fragment class where declaring buttons and their on click events breaks the app

--- app/src/main/java/com/example/buzzinbees/HomeFragment.java
+++ app/src/main/java/com/example/buzzinbees/HomeFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,9 +27,9 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-
-
-
+//        Fragment videoFragment = new MusicControls();
+//        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+//        transaction.add(R.id.songControls_frag, videoFragment).commit();
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home,null);
--- app/src/main/java/com/example/buzzinbees/MusicControls.java
+++ app/src/main/java/com/example/buzzinbees/MusicControls.java
@@ -22,80 +22,80 @@ public class MusicControls extends Fragment {
 
     SeekBar songSeekBar;
 
-    public static MusicControls newInstance() {
-        return new MusicControls();
-    }
+//    public static MusicControls newInstance() {
+//        return new MusicControls();
+//    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
-        // find all the views and set their onClick listener
-        connectBluetooth = getView().findViewById(R.id.btn_Connect);
-        connectBluetooth.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // call the bluetooth class to create the connection
-            }
-        });
-
-        callVisualizer = getView().findViewById(R.id.btn_toVisualizer);
-        callVisualizer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // call the visualizer fragment
-            }
-        });
-
-        musicQueue = getView().findViewById(R.id.btn_toQueue);
-        musicQueue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // go to the playlists fragment and show the current music queue
-            }
-        });
-
-        playPauseBtn = getView().findViewById(R.id.btn_PlayPause);
-        playPauseBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // switch play/pause audio
-            }
-        });
-
-        nextBtn = getView().findViewById(R.id.btn_Next);
-        nextBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // go to the next song in the queue
-            }
-        });
-
-        prevBtn = getView().findViewById(R.id.btn_Prev);
-        prevBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // go to the previous song in the queue
-            }
-        });
-
-        shflBtn = getView().findViewById(R.id.btn_Shuffle);
-        shflBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // shuffle the queue
-            }
-        });
-
-        loopBtn = getView().findViewById(R.id.btn_Loop);
-        loopBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // loop the queue (copy the current queue and add it to the end)
-            }
-        });
-
-        songSeekBar = getView().findViewById(R.id.song_seekBar);
+//        // find all the views and set their onClick listener
+//        connectBluetooth = getView().findViewById(R.id.btn_Connect);
+//        connectBluetooth.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // call the bluetooth class to create the connection
+//            }
+//        });
+//
+//        callVisualizer = getView().findViewById(R.id.btn_toVisualizer);
+//        callVisualizer.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // call the visualizer fragment
+//            }
+//        });
+//
+//        musicQueue = getView().findViewById(R.id.btn_toQueue);
+//        musicQueue.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // go to the playlists fragment and show the current music queue
+//            }
+//        });
+//
+//        playPauseBtn = getView().findViewById(R.id.btn_PlayPause);
+//        playPauseBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // switch play/pause audio
+//            }
+//        });
+//
+//        nextBtn = getView().findViewById(R.id.btn_Next);
+//        nextBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // go to the next song in the queue
+//            }
+//        });
+//
+//        prevBtn = getView().findViewById(R.id.btn_Prev);
+//        prevBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // go to the previous song in the queue
+//            }
+//        });
+//
+//        shflBtn = getView().findViewById(R.id.btn_Shuffle);
+//        shflBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // shuffle the queue
+//            }
+//        });
+//
+//        loopBtn = getView().findViewById(R.id.btn_Loop);
+//        loopBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // loop the queue (copy the current queue and add it to the end)
+//            }
+//        });
+//
+//        songSeekBar = getView().findViewById(R.id.song_seekBar);
 
 
         return inflater.inflate(R.layout.music_controls_fragment, container, false);
