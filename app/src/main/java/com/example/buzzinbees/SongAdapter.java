b585517c4e7fd71ec122093026ed26640656eb32
DawnEggleton <dawn.eggleton@carleton.ca>
2020-02-10 09:51:31 -0500

    ListView Integrated

---
+++ app/src/main/java/com/example/buzzinbees/SongAdapter.java
@@ -0,0 +1,36 @@
+package com.example.buzzinbees;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class SongAdapter extends ArrayAdapter<Song> {
+
+    //constructor DO NOT DELETE
+    public SongAdapter(Context context, ArrayList<Song> songs) {
+        super(context, 0, songs);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //get data for snog
+        Song song = getItem(position);
+        //checked if the song_view is reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.view_song, parent, false);
+        }
+
+        //look up view and populate data
+        TextView tvSong = convertView.findViewById(R.id.txt_SongTitle);
+        TextView tvArtist = convertView.findViewById(R.id.txt_ArtistName);
+        tvSong.setText(song.songName);
+        tvArtist.setText(song.songArtist);
+        return convertView;
+    }
+
+}
